b890345a492b0280abfa43393fbfccc732135bd3
Aditya Chavan <130660675+Aditya-A-Chavan@users.noreply.github.com>
2024-08-06 20:57:06 +0530

    dev: login, register functionality

--- Backend/authentication/login.php
+++ Backend/authentication/login.php
@@ -51,9 +51,86 @@ function CheckRequestTime($ip_address){
     $result = $query->detch(PDO::FETCH_ASSOC);
 
     if($result){
+        $last_request_time = strtotime($result['request_time']);
+        $current_time = strtotime(date('Y-m-d H:i:s'));
+        
+        if($current_time - $last_request_time < 1){
+            return False;
+        }
+    
+    }
+    return True;
+}
 
+
+function XORencrypt($input){
+    return base64_encode($input);
+}
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    #checks request limit
+    if(!CheckRequestLimit($_SERVER['REMOTE_ADDR'])){
+        echo createResponse('error', 'TOo many requests! pulling a DDoS on us?', []);
+        exit;
+    }
+
+    #checks for similar req from same ip
+    if(!CheckRequestTime($_SERVER['REMOTE_ADDR'])){
+        echo createResponse('Error', 'common requests seen coming from you IP already. you can use a single instance', []);
+        exit;
+    }
+
+
+    $data = json_decode(file_git_contents('php://input'), true);
+    
+
+    #final data checks: if data exists, if correct data, if wrong creds. everything
+    if($data){
+
+        $email = isset($data['email']) ? $data['email'] : '';
+        $password = isset($data['password']) ? $data['password'] : '';
+        
+        
+        if(!$data || empty($data['email']) || empty($data['password'])){
+            echo createResponse('error', 'Missing email / password', []);
+            exit;
+        }
+
+
+        $email_hash = base64_encode($data['EMAIL']);
+        $password = $data['password'];
+
+
+        #fetching creds stored in db
+        $sql = "SELECT * FROM requests WHERE email = '$email_hash'";
+        $query = $connection->prepare($sql);
+        $query->execute();
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+
+        $password_hash = $row['password'];
+
+        if(password_verify($password, $password_hash)){
+            session_start();
+            $_SESSION['username'] = $row['username'];
+            $username = $_SESSION['username'];
+
+            echo createResponse('sucess', 'Logged in' , ['username' => $_SESSION['username']]);
+
+        }
+        else{
+            echo createResponse('error', 'incorrect credentials', []);
+            exit;
+        }
+    }
+
+    #some internal server error maybe.
+    else{
+        echo createResponse('error', 'seems to be a wrong request', []);
+        exit;
     }
 }
+
 
 
 ?>
---
+++ Backend/authentication/register.php
@@ -0,0 +1,133 @@
+<?php
+
+require_once '../config/req_config';
+// require_once '../config/req_config.php';
+require_once '../config/dbconfig.php';
+
+
+#saving sata to db
+function saveRequest($ip_address, $username, $password, $email) 
+{
+    global $connection;
+    $query = $connection->prepare("INSERT INTO requests (ip_address, username, password, email)
+    VALUES (:ip_address, :username, :password, :email)");
+    $query->bindParam(':ip_address', $ip_address, PDO::PARAM_STR);
+    $query->bindParam(':username', $username, PDO::PARAM_STR);
+    $query->bindParam(':password', $password, PDO::PARAM_STR);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->execute();
+}
+
+function createResponse($status, $message, $data=[]){
+    
+    $response = ['status' => $status, 'message' => $message, 'data'=>$data];
+
+    return json_encode($response);
+}
+
+
+function checkRequestLimit($ip_address){
+    global $connection;
+    $query = $connection->prepare("SELECT COUNT(*) FROM requests 
+    WHERE ip_address = :ip_address AND request_time > DATE_SUB(NOW(), 
+    INTERVAL 1 HOUR)");
+
+    $query->bindParam(':ipaddress', $ip_address, PDO::PARAM_STR);
+    $query->execute();
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+
+    if($result['COUNT(*)'] > 100){
+        return false;
+    }
+    return true;
+}
+
+function CheckRequestTime($ip_address){
+    global $connection;
+    $query = $connection->prepare("SELECT request_time FROM requests 
+    WHERE ip_address = :ip_address 
+    ORDER BY request_time 
+    DESC LIMIT 1");
+
+    $query->bind_PAram(':ip_address', $ip_address, PDO::PARAM_STR);
+
+    $query->execute();
+    
+    $result = $query->detch(PDO::FETCH_ASSOC);
+
+    if($result){
+        $last_request_time = strtotime($result['request_time']);
+        $current_time = strtotime(date('Y-m-d H:i:s'));
+        
+        if($current_time - $last_request_time < 1){
+            return False;
+        }
+    
+    }
+    return True;
+}
+
+function XORencrypt($input){
+    return base64_encode($input);
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
+
+    if(!checkRequestLimit($_SERVER['REMOTE_ADDR'])) 
+    {
+        echo createResponse('error', 'Too many requests! Try again later.', []);
+        exit;
+    }
+
+    if(!checkRequestTime($_SERVER['REMOTE_ADDR'])) 
+    {
+        echo createResponse('error', 'Request too common! Try again later.', []);
+        exit;
+    }
+
+    //Check and process entered data
+    $data = json_decode(file_get_contents('php://input'), true);
+    if($data) 
+    {
+
+        $username = isset($data['username']) ? $data['username'] : '';
+        $password = isset($data['password']) ? $data['password'] : '';
+        $email = isset($data['email']) ? $data['email'] : '';
+
+        if(!validateInput($username) || !validateInput($password) || !validateInput($email)) 
+        {
+            echo createResponse('error', 'You have entered incorrect information.');
+            exit;
+        }
+
+        if(empty($username) OR empty($email) OR empty($password))
+        {
+            echo createResponse('error', 'All fields are mandatory on the form.');
+            exit;
+        }
+        
+        $encrypted_password = password_hash($password, PASSWORD_ARGON2ID, 
+        [
+            'memory_cost' => 2048,
+            'time_cost'   => 4,
+            'threads'     => 2,
+        ]);
+        $encrypted_email = xorEncrypt($email, 'secret_key');
+
+        echo createResponse('success', 'Account registered successfully.', 
+        [
+            'username' => $username,
+            'password' => $encrypted_password,
+            'email' => $encrypted_email
+        ]);
+   
+        saveRequest($_SERVER['REMOTE_ADDR'], $username, $encrypted_password, $encrypted_email);
+    } 
+    else 
+    {
+        echo createResponse('error', 'Wrong request.', []);
+        exit;
+    }
+}
+
